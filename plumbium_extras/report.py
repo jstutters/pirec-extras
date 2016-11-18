f3c98aa860b553ffcbf228faef1ce40db1ccc7f5
Jon Stutters <j.stutters@ucl.ac.uk>
2016-11-18 15:26:23 +0000

    Move parsing of dict paths to separate function

--- plumbium_extras/report.py
+++ plumbium_extras/report.py
@@ -5,6 +5,16 @@ import tarfile
 ARRAY_EXP = r'(?P<key>^[A-Za-z][A-Za-z0-9_\-]*)(?P<index>\[[0-9]+\])'
 
 
+def walk_path(path):
+    for p in path.split('.'):
+        m = re.match(ARRAY_EXP, p)
+        if m is not None:
+            index = int(m.group('index')[1:-1])
+            yield (m.group('key'), index)
+        else:
+            yield (p, None)
+
+
 def get_report_json(tf):
     for member in tf:
         if member.name.endswith('report.json'):
@@ -13,14 +23,11 @@ def get_report_json(tf):
 
 def get_from_dict_by_path(jsondoc, keypath):
     c = jsondoc
-    for p in keypath.split('.'):
-        m = re.match(ARRAY_EXP, p)
-        if m is not None:
-            index = int(m.group('index')[1:-1])
-            keyval = c.get(m.group('key'))
-            c = keyval[index]
+    for key, index in walk_path(keypath):
+        if index is not None:
+            c = c.get(key)[index]
         else:
-            c = c.get(p)
+            c = c.get(key)
     return c
 
 
