c10b0e0268e580acd1e2d67be47deed909c0ab87
Jon Stutters <j.stutters@ucl.ac.uk>
2016-12-22 09:56:46 +0000

    Use underscore version of module name

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pbtool = plumbium-extras.cli:cli'
+            'pbtool=plumbium_extras.cli:cli'
         ]
     },
     license='MIT',
